c467fe2bcec94af967f6ad30f7db38c6b5854420
Leslie Shea <leslieyshea@gmail.com>
2019-05-17 15:45:42 -0700

    added paging

--- src/containers/quotes/AllCharacters.js
+++ src/containers/quotes/AllCharacters.js
@@ -2,23 +2,54 @@ import React, { PureComponent } from 'react';
 import Characters from '../../components/characters/Characters';
 import Loading from '../../components/loading/Loading';
 import { getCharacters } from '../../services/rickAndMortyApi';
+import Paging from '../../components/paging/Paging';
 
 export default class AllCharacters extends PureComponent {
   state = {
+    currentPage: 1,
+    totalPages: 1,
     characters: [],
     loading: true
   }
 
   fetchCharacters = () => {
     this.setState({ loading: true });
-    getCharacters()
-      .then(({ characters }) => this.setState({ characters, loading: false }));
+    getCharacters(this.state.currentPage)
+      .then(({ totalPages, characters }) => this.setState({
+        totalPages,
+        characters,
+        loading: false
+      }));
+  }
+
+  nextPage = page => {
+    this.setState({ currentPage: page });
+  }
+
+  componentDidMount() {
+    this.fetchCharacters();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.currentPage !== this.state.currentPage) {
+      this.fetchCharacters();
+    }
   }
 
   render() {
-    const { characters, loading } = this.state;
+    const {
+      currentPage,
+      totalPages,
+      characters,
+      loading
+    } = this.state;
+
     if(loading) return <Loading />;
 
-    return <Characters characters={characters} />;
+    return (
+      <Paging currentPage={currentPage} totalPages={totalPages} nextPage={this.nextPage}>
+        <Characters characters={characters} />
+      </Paging>
+    );
   }
 }
