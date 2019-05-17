9409741904f23ff16b24f5a4291777443bfa33da
Leslie Shea <leslieyshea@gmail.com>
2019-05-17 15:59:43 -0700

    fixed tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import Header from './header/Header';
 import AllCharacters from '../containers/quotes/AllCharacters';
 
 export default function App() {
-  return <AllCharacters />;
+  return (
+    <>
+      <Header />
+      <AllCharacters />
+    </>
+  );
 }
